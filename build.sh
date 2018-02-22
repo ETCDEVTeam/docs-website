427db7837ef2d13754f86f16755e42e001ec0b11
ia <isaac.ardis@gmail.com>
2018-02-22 19:22:37 +0900

    refactor, add comments

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# Fail at first error
+set -e
+
 PWD=$(pwd)
-SOURCES=$PWD/_sources
-TARGET=$PWD/_target
+SOURCES=$PWD/_sources # Where raw repos will be clone to
+TARGET=$PWD/_target # Where documentation will build to
 
 # Allows to switch between ssh and https cloning/pulling.
 GIT_REMOTE_BASE_URL_SSH=git@github.com:
@@ -11,12 +14,25 @@ GIT_REMOTE_BASE_URL=$GIT_REMOTE_BASE_URL_SSH
 
 # Establish external dependencies
 declare -a deps=(gitbook ebook-convert)
+function depusage() {
+		echo "\
+To install missing dependencies, run
+
+	$ npm install -g $1
+
+Or, if missing 'ebook-convert' on MacOs...
+
+	$ brew cask install calibre
+
+"
+}
+
+# Whitelist projects
 declare -a projects=(
 	ETCDEVTeam/emerald-cli 
 	ethereumproject/go-ethereum 
-	whilei/go-ethereum
+	whilei/go-ethereum # just for dev purposes
 )
-
 function printprojects() {
 	local out="
 "
@@ -27,66 +43,113 @@ function printprojects() {
 	echo "$out"
 }
 
+# Print help/usage in case of invalid use
+function usage() {
+		echo "Use:"
+		echo "--https             : enable https git scheme instead of ssh default"
+		echo " -B                 : build docs for all available projects
+
+	Available projects are: $(printprojects)
+
+"
+		echo " -b <project>       : build docs for a single project or multiple
+
+	To build for specific single or multiple projects, use '-b project1 -b project2'
+"
+		echo " -w                 : build website"
+		echo " -d                 : deploy"
+
+		echo "\
+
+Examples:
+
+	$ ./build.sh --https -b emerald-cli
+	$ ./build.sh -b emerald-cli -b go-ethereum
+
+"
+}
+
+# Create source and target dirs if not exist
 [ -d "$SOURCES" ] || mkdir -p "$SOURCES"
 [ -d "$TARGET/html" ] || mkdir -p "$TARGET/html"
 [ -d "$TARGET/pdf" ] || mkdir -p "$TARGET/pdf"
 [ -d "$TARGET/epub" ] || mkdir -p "$TARGET/epub"
 
-set -e
+function clone_or_pull_projects() {
+	local PROJECT="$1"
+	echo "-> Git URL: $GIT_REMOTE_BASE_URL$PROJECT.git"
+	echo "-> Source: $SOURCES/$PROJECT"
 
-function get_projects() {
-    PROJECT=$1
-    echo "Get projects $GIT_REMOTE_BASE_URL/$PROJECT.git at $SOURCES"
-
-    cd "$SOURCES"
-	if [ ! -d "$TARGET/$PROJECT" ]; then
-    	mkdir -p "$TARGET/$PROJECT"
-	fi
-
-    if [ -d "./$PROJECT" ]; then
-        cd "$PROJECT"
-        if git pull origin master; then
+	# If sources project dir exists, git pull update
+	if [ -d "$SOURCES/$PROJECT" ]; then
+		if git -C "$SOURCES/$PROJECT" pull origin master; then
 			echo Success.
 		else
-			echo Could not pull from origin master for existing repo.
-			cd "$SOURCES"
-			echo pwd "$(pwd)"
-			echo removing "./$PROJECT"
-			rm -rf "./$PROJECT"
-			mkdir -p "./$PROJECT/.."
+			# There was an error... perhaps something like git remote name has changed...
+			echo Could not pull from origin master for existing repo "$SOURCES/$PROJECT".
+	
+			echo Removing "$SOURCES/$PROJECT"
+			rm -rf "$SOURCES/$PROJECT"
+	
+			# Make just the owner namespace dir (since we'll clone)
+			mkdir -p "$SOURCES/$PROJECT/.." 
 
-			# TODO: refactor me DRY
-			if git clone "$GIT_REMOTE_BASE_URL""$PROJECT.git"; then
+			cd "$SOURCES/$PROJECT/.." # owner namespace, eg. $SOURCES/ethereumproject/
+			if git clone "$GIT_REMOTE_BASE_URL$PROJECT.git"; then
 				echo Success.
+			else
+				echo Used remote url: "$GIT_REMOTE_BASE_URL$PROJECT.git"
+				echo Could not clone to "$SOURCES/$PROJECT"
+				exit 1
 			fi
 		fi
-    else
-        if git clone "$GIT_REMOTE_BASE_URL""$PROJECT.git"; then
+	else
+		mkdir -p "$SOURCES/$PROJECT/.."
+		cd "$SOURCES/$PROJECT/.." # owner namespace, eg. $SOURCES/ethereumproject/
+		if git clone "$GIT_REMOTE_BASE_URL$PROJECT.git"; then
 			echo Success.
 		fi
-    fi
+	fi
 }
 
 function build_docs() {
-    PROJECT=$1
-	mkdir -p "$SOURCES/$PROJECT"
-    cd "$SOURCES/$PROJECT"
+	local PROJECT="$1"
+	echo "-> Using Gitbook to bind $PROJECT..."
 
-    gitbook build
-    rm -rf "$TARGET/html/$PROJECT/"
-    mv _book/ "$TARGET/html/$PROJECT/"
+	# Ensure Gitbook required files exist.
+	if [ ! -f "$SOURCES/$PROJECT/book.json" ]; then
+		echo "Missing required $SOURCES/$PROJECT/book.json file. Gitbook cannot continue."
+		exit 1
+	fi
+	# TODO: refactor docs dir to script var?
+	if [ ! -f "$SOURCES/$PROJECT/docs/README.md" ] && [ ! -f "$SOURCES/$PROJECT/docs/README.adoc" ]; then
+		echo "Missing required $SOURCES/$PROJECT/docs/README.(md|adoc) file. Gitbook cannot continue."
+		exit 1
+	fi
+	
+	# Get just the repo name, splitting on '/' from the namespace prefix
+	IFS='/' read -ra ARR <<< "$PROJECT"
+	local REPONAME="${ARR[1]}"
+	echo "-> Repo name: $REPONAME"
 
-    rm -rf "$TARGET/pdf/$PROJECT.pdf"
-    gitbook pdf ./ "$TARGET/pdf/$PROJECT.pdf"
+	echo "-> Target: $TARGET/html/$REPONAME/"
+	gitbook build "$SOURCES/$PROJECT"
+	rm -rf "$TARGET/html/$REPONAME/"
+	mv "$SOURCES/$PROJECT/_book/" "$TARGET/html/$REPONAME/"
 
-    rm -rf "$TARGET/epub/$PROJECT.epub"
-    gitbook epub ./ "$TARGET/epub/$PROJECT.epub"
+	echo "-> Target: $TARGET/pdf/$REPONAME/"
+	rm -rf "$TARGET/pdf/$REPONAME.pdf"
+	gitbook pdf "$SOURCES/$PROJECT/." "$TARGET/pdf/$REPONAME.pdf"
 
-    git checkout book.json
+	echo "-> Target: $TARGET/epub/$REPONAME/"
+	rm -rf "$TARGET/epub/$REPONAME.epub"
+	gitbook epub "$SOURCES/$PROJECT/." "$TARGET/epub/$REPONAME.epub"
+
+	git -C "$SOURCES/$PROJECT" checkout "$SOURCES/$PROJECT/book.json"
 }
 
-function build() {
-    PROJECT=$1
+function get_and_build() {
+	local PROJECT="$1"
 
 	# Ensure project is whitelisted.
 	local match=0
@@ -101,59 +164,23 @@ function build() {
 		exit 1
 	fi
 
-    get_projects "$PROJECT"
-    build_docs "$PROJECT"
+	clone_or_pull_projects "$PROJECT"
+	build_docs "$PROJECT"
 }
 
 function deploy() {
-    BUCKET="docs.etcdevteam.com"
-    gsutil -m rsync -R "$TARGET" "gs://$BUCKET"
-    gsutil -m acl ch -u "AllUsers:R" -r "gs://$BUCKET"
-    gsutil -m setmeta -h "Cache-Control:public, max-age=900" -r "gs://$BUCKET"
+	BUCKET="docs.etcdevteam.com"
+	gsutil -m rsync -R "$TARGET" "gs://$BUCKET"
+	gsutil -m acl ch -u "AllUsers:R" -r "gs://$BUCKET"
+	gsutil -m setmeta -h "Cache-Control:public, max-age=900" -r "gs://$BUCKET"
 }
 
-function usage() {
-        echo "Use:"
-		echo "--https             : enable https git scheme instead of ssh default"
-		echo " -B                 : build docs for all available projects
-
-	Available projects are: $(printprojects)
-
-"
-        echo " -b <project>       : build docs for a single project or multiple
-
-	To build for specific single or multiple projects, use '-b project1 -b project2'
-"
-        echo " -w                 : build website"
-        echo " -d                 : deploy"
-
-		echo "\
-
-Examples:
-
-	$ ./build.sh --https -b emerald-cli
-	$ ./build.sh -b emerald-cli -b go-ethereum
-
-"
-}
-
-function depusage() {
-		echo "\
-To install missing dependencies, run
-
-	$ npm install -g $1
-
-Or, if missing 'ebook-convert' on MacOs...
-
-	$ brew cask install calibre
-
-"
-}
+# Let the processing begin.
 
 # Check for minimum arguments existing.
 if [ $# -eq 0 ]
   then
-    echo "No arguments supplied"
+	echo "No arguments supplied"
 	usage
 	exit 1
 fi
@@ -166,39 +193,41 @@ done
 # Parse git scheme.
 case "$1" in
 	--https)
-		echo "Using https git scheme"
+		echo "-> Git scheme: https"
 		GIT_REMOTE_BASE_URL=$GIT_REMOTE_BASE_URL_HTTPS
 		shift
 		;;
+	*)
+		echo "-> Git scheme: ssh"
+		;;
 esac
 
+# Parse options.
 while getopts ":b:Bwd" opt; do
   case $opt in
-    b)
+	b)
 		echo "\
--> Building docs: $OPTARG"
-		build "$OPTARG"
+-> Building docs for: $OPTARG"
+		get_and_build "$OPTARG"
 		;;
 	B)
 		echo "Building all docs: $(printprojects)"
 		for p in "${projects[@]}"; do
-			echo "Building $p ..."
-			build "$p"
+			echo "Building docs for $p ..."
+			get_and_build "$p"
 		done
 		;;
 	w)
-        echo "Build main website"
-        node webpack.js --no-watch --minimize
-        ;;
-    d)
-        echo "Deploy docs to https://docs.etcdevteam.com"
-        deploy
-        ;;
-    *)  echo "Unknown $1"
+		echo "Build main website"
+		node webpack.js --no-watch --minimize
+		;;
+	d)
+		echo "Deploy docs to https://docs.etcdevteam.com"
+		deploy
+		;;
+	*)  echo "Unknown $1"
 		usage
-        exit 1
-        ;;
+		exit 1
+		;;
   esac
-#   shift
 done
-
