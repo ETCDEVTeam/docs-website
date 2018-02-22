e464a74dd8570a99698eaf789038ffff8a98b557
ia <isaac.ardis@gmail.com>
2018-02-22 18:11:30 +0900

    fix linter errors

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-ME=$(pwd)
-SOURCES=$ME/_sources
-TARGET=$ME/_target
+PWD=$(pwd)
+SOURCES=$PWD/_sources
+TARGET=$PWD/_target
+
 # Allows to switch between ssh and https cloning/pulling.
 GIT_REMOTE_BASE_URL_SSH=git@github.com:
 GIT_REMOTE_BASE_URL_HTTPS=https://github.com/
@@ -10,48 +11,76 @@ GIT_REMOTE_BASE_URL=$GIT_REMOTE_BASE_URL_SSH
 
 # Establish external dependencies
 declare -a deps=(gitbook ebook-convert)
-declare -a sources=(emerald-cli go-ethereum)
+declare -a projects=(
+	ETCDEVTeam/emerald-cli 
+	ethereumproject/go-ethereum 
+	whilei/go-ethereum
+)
 
-[ -d $SOURCES ] || mkdir -p $SOURCES
+function printprojects() {
+	local out="
+"
+	for p in "${projects[@]}"; do
+		out="$out
+	$p"
+	done
+	echo "$out"
+}
+
+[ -d "$SOURCES" ] || mkdir -p "$SOURCES"
 [ -d "$TARGET/html" ] || mkdir -p "$TARGET/html"
 [ -d "$TARGET/pdf" ] || mkdir -p "$TARGET/pdf"
 [ -d "$TARGET/epub" ] || mkdir -p "$TARGET/epub"
 
 set -e
 
-function get_sources() {
+function get_projects() {
     PROJECT=$1
-    echo "Get sources $GIT_REMOTE_BASE_URL""ETCDEVTeam/$PROJECT.git at $SOURCES"
+    echo "Get projects $GIT_REMOTE_BASE_URL/$PROJECT.git at $SOURCES"
 
-    cd $SOURCES
-    mkdir -p $TARGET/$PROJECT
+    cd "$SOURCES"
+	if [ ! -d "$TARGET/$PROJECT" ]; then
+    	mkdir -p "$TARGET/$PROJECT"
+	fi
 
     if [ -d "./$PROJECT" ]; then
-        cd $PROJECT
-        git pull origin master
-    else
-        if git clone "$GIT_REMOTE_BASE_URL"ETCDEVTeam/"$PROJECT.git"; then
-			echo Got it.
+        cd "$PROJECT"
+        if git pull origin master; then
+			echo Success.
 		else
-			echo "Failed to clone from ETCDEVTEAM, trying ethereumproject/..."
-			git clone "$GIT_REMOTE_BASE_URL"ethereumproject/"$PROJECT.git"
+			echo Could not pull from origin master for existing repo.
+			cd "$SOURCES"
+			echo pwd "$(pwd)"
+			echo removing "./$PROJECT"
+			rm -rf "./$PROJECT"
+			mkdir -p "./$PROJECT/.."
+
+			# TODO: refactor me DRY
+			if git clone "$GIT_REMOTE_BASE_URL""$PROJECT.git"; then
+				echo Success.
+			fi
+		fi
+    else
+        if git clone "$GIT_REMOTE_BASE_URL""$PROJECT.git"; then
+			echo Success.
 		fi
     fi
 }
 
 function build_docs() {
     PROJECT=$1
-    cd $SOURCES/$PROJECT
+	mkdir -p "$SOURCES/$PROJECT"
+    cd "$SOURCES/$PROJECT"
 
     gitbook build
-    rm -rf $TARGET/html/$PROJECT/
-    mv _book/ $TARGET/html/$PROJECT/
+    rm -rf "$TARGET/html/$PROJECT/"
+    mv _book/ "$TARGET/html/$PROJECT/"
 
-    rm -rf $TARGET/pdf/$PROJECT.pdf
-    gitbook pdf ./ $TARGET/pdf/$PROJECT.pdf
+    rm -rf "$TARGET/pdf/$PROJECT.pdf"
+    gitbook pdf ./ "$TARGET/pdf/$PROJECT.pdf"
 
-    rm -rf $TARGET/epub/$PROJECT.epub
-    gitbook epub ./ $TARGET/epub/$PROJECT.epub
+    rm -rf "$TARGET/epub/$PROJECT.epub"
+    gitbook epub ./ "$TARGET/epub/$PROJECT.epub"
 
     git checkout book.json
 }
@@ -61,24 +90,24 @@ function build() {
 
 	# Ensure project is whitelisted.
 	local match=0
-	for s in "${sources[@]}"; do
+	for s in "${projects[@]}"; do
 		if [ "$s" == "$PROJECT" ]; then
 			match=1
 		fi
 	done
 	if [ $match -eq 0 ]; then
 		echo "Unknown source $PROJECT"
-		echo "Known projects are: ${sources[@]}"
+		echo "Known projects are: $(printprojects)"
 		exit 1
 	fi
 
-    get_sources "$PROJECT"
+    get_projects "$PROJECT"
     build_docs "$PROJECT"
 }
 
 function deploy() {
     BUCKET="docs.etcdevteam.com"
-    gsutil -m rsync -R $TARGET "gs://$BUCKET"
+    gsutil -m rsync -R "$TARGET" "gs://$BUCKET"
     gsutil -m acl ch -u "AllUsers:R" -r "gs://$BUCKET"
     gsutil -m setmeta -h "Cache-Control:public, max-age=900" -r "gs://$BUCKET"
 }
@@ -88,7 +117,7 @@ function usage() {
 		echo "--https             : enable https git scheme instead of ssh default"
 		echo " -B                 : build docs for all available projects
 
-	Available projects are: [${sources[@]}]
+	Available projects are: $(printprojects)
 
 "
         echo " -b <project>       : build docs for a single project or multiple
@@ -151,8 +180,8 @@ while getopts ":b:Bwd" opt; do
 		build "$OPTARG"
 		;;
 	B)
-		echo "Building all docs: ${sources[@]}"
-		for p in "${sources[@]}"; do
+		echo "Building all docs: $(printprojects)"
+		for p in "${projects[@]}"; do
 			echo "Building $p ..."
 			build "$p"
 		done
